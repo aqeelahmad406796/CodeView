bc59a2088ba22e4bf06c366c47748ea1622d09da
AugmentCare <aqeel@augmentcare.com>
2018-10-17 23:26:11 +0500

    Initial Commit

---
+++ CodeScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeScreen
+//
+//  Created by AugmentCare on 17/10/2018.
+//  Copyright © 2018 AstuteSoftwares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
